8ba516012d0894029be89d86fda0193e875c92b9
M-Ajay-Yadav <153363272+M-Ajay-Yadav@users.noreply.github.com>
2024-08-27 21:40:17 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(`${BASE_URL}/tasks`, taskRoutes);
+app.use("/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 
